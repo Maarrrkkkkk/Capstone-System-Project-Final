58d3b6880356b5f01e7e27c3eb78add1c3269367
Mark Anthony Aguirre <153790909+Maarrrkkkkk@users.noreply.github.com>
2024-08-08 13:23:51 +0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -15,3 +15,28 @@ class CustomUser(AbstractUser):
         return self.username
     
 
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+
+class Group(models.Model):
+    member1 = models.CharField(max_length=100)
+    member2 = models.CharField(max_length=100)
+    member3 = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+
+class GroupInfoTH(models.Model):
+    member1 = models.CharField(max_length=100, null=True, blank=True)
+    member2 = models.CharField(max_length=100, null=True, blank=True)
+    member3 = models.CharField(max_length=100, null=True, blank=True)
+    section = models.CharField(max_length=50)
+    subject_teacher = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+class Schedule(models.Model):
+    date = models.DateField()
+    slot = models.TimeField()
+    room = models.CharField(max_length=100)
+    faculty1 = models.ForeignKey(Faculty, related_name='faculty1', on_delete=models.CASCADE)
+    faculty2 = models.ForeignKey(Faculty, related_name='faculty2', on_delete=models.CASCADE)
+    faculty3 = models.ForeignKey(Faculty, related_name='faculty3', on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
